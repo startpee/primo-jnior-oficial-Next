90b5d28a7e35b1a50fad3fd091a5922018a56c5c
Startpee <86379169+startpee@users.noreply.github.com>
2022-12-30 13:55:13 -0300

    Commit from TeleportHQ

---
+++ pages/coruja.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Coruja = (props) => {
+  return (
+    <>
+      <div className="coruja-container">
+        <Head>
+          <title>coruja - O Primo Júnior | Sabedoria jovem</title>
+          <meta
+            name="description"
+            content="meus vídeos pode te ajudar a despertar o seu potencial como pessoa.\n\n"
+          />
+          <meta
+            property="og:title"
+            content="coruja - O Primo Júnior | Sabedoria jovem"
+          />
+          <meta
+            property="og:description"
+            content="meus vídeos pode te ajudar a despertar o seu potencial como pessoa.\n\n"
+          />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .coruja-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Coruja
