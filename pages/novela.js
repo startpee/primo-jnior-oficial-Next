e52552a130b46d299dbb106d6eaa5174c1cbbd2d
Startpee <86379169+startpee@users.noreply.github.com>
2022-12-30 14:37:12 -0300

    Commit from TeleportHQ

--- pages/novela.js
+++ pages/novela.js
@@ -26,6 +26,24 @@ const Novela = (props) => {
           src="/playground_assets/brisa-travessia-1600x900-1122-1500w.jpg"
           className="novela-image"
         />
+        <h1 className="novela-text1">Foto de frente</h1>
+        <img
+          alt="image"
+          src="/playground_assets/img_20221230_094750%20%5B1%5D-1500w.jpg"
+          className="novela-image1"
+        />
+        <h1 className="novela-text2">Foto de trás</h1>
+        <img
+          src="/playground_assets/img_20221230_094750-removebg-preview-1500w.png"
+          alt="image"
+          className="novela-image2"
+        />
+        <a
+          href="https://api.whatsapp.com/send?phone=5511967272695"
+          className="novela-link button"
+        >
+          Comprar agora
+        </a>
       </div>
       <style jsx>
         {`
@@ -48,16 +66,74 @@ const Novela = (props) => {
             height: 470px;
             object-fit: cover;
           }
+          .novela-text1 {
+            font-size: 44px;
+            margin-top: var(--dl-space-space-threeunits);
+            margin-bottom: var(--dl-space-space-unit);
+          }
+          .novela-image1 {
+            width: 788px;
+            height: 1082px;
+            object-fit: cover;
+            margin-bottom: var(--dl-space-space-halfunit);
+          }
+          .novela-text2 {
+            font-size: 44px;
+            margin-top: var(--dl-space-space-threeunits);
+            margin-bottom: var(--dl-space-space-unit);
+          }
+          .novela-image2 {
+            width: 920px;
+            height: 1192px;
+            object-fit: cover;
+          }
+          .novela-link {
+            color: var(--dl-color-gray-white);
+            width: 289px;
+            height: 85px;
+            font-size: 24px;
+            margin-top: var(--dl-space-space-unit);
+            text-align: center;
+            line-height: 2;
+            border-color: #ffffff;
+            border-width: 3px;
+            border-radius: var(--dl-radius-radius-radius8);
+            margin-bottom: var(--dl-space-space-unit);
+            text-decoration: none;
+            background-color: rgb(23, 22, 32);
+          }
           @media (max-width: 767px) {
             .novela-image {
               width: 100%;
               height: 448px;
+            }
+            .novela-link {
+              color: var(--dl-color-gray-white);
+              font-size: 24px;
+              text-align: center;
+              text-decoration: none;
+              background-color: rgb(23, 22, 32);
             }
           }
           @media (max-width: 479px) {
             .novela-image {
               width: 100%;
               height: 366px;
+            }
+            .novela-image1 {
+              width: 100%;
+              height: 900px;
+            }
+            .novela-image2 {
+              width: 100%;
+              height: 774px;
+            }
+            .novela-link {
+              color: var(--dl-color-gray-white);
+              font-size: 24px;
+              line-height: 2;
+              text-decoration: none;
+              background-color: rgb(23, 22, 32);
             }
           }
         `}
